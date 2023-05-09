17799979e7fd8a49cabd9c7d6137508b5e2c6b87
Joshgun RZABAYLI <joshgun.rzabayli@gmail.com>
2023-05-10 00:27:24 +0400

    Test Images were added.

--- myproject/cryptoApi/urls.py
+++ myproject/cryptoApi/urls.py
@@ -5,5 +5,5 @@ from .views import list_addresses, generate_address, retrieve_address
 urlpatterns = [
     path('generate_address/', generate_address, name='generate_address'),
     path('list_addresses/', list_addresses, name='list_addresses'),
-    path('retrieve_address/<int:address_id>/', retrieve_address, name='retrieve_address'),
+    path('retrieve_address/<int:id>/', retrieve_address, name='retrieve_address'),
 ]
